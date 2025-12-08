85f060d19c52ccc4bd3b76abec77a256156f6f07
zumba <zumba@iki.fi>
2025-12-08 14:50:03 +0200

    Change quotes to match coding style

--- src/TokenMapper.php
+++ src/TokenMapper.php
@@ -54,7 +54,7 @@ final class TokenMapper
             throw new RuntimeException(sprintf('File [%s] not found', $this->path()));
         }
 
-        fputcsv($file, [$requestId, $debugToken], escape: "");
+        fputcsv($file, [$requestId, $debugToken], escape: '');
         fclose($file);
     }
 
@@ -73,7 +73,7 @@ final class TokenMapper
             throw new RuntimeException(sprintf('File [%s] not found', $this->path()));
         }
 
-        while ($row = fgetcsv($file, escape: "")) {
+        while ($row = fgetcsv($file, escape: '')) {
             /** @var array{0: string, 1: string} $row */
             $map[$row[0]] = $row[1];
         }
